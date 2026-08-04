9c67985fbc3b27cad2e50b9e15f6dc8f77106862
ntshuxeko shirinda <ntshuxekoshirinda96@gmail.com>
2026-08-04 21:05:31 +0200

    Add GitHub Actions CI/CD pipeline for Terraform

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-production"
+  location = "South Africa North"
+}
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-production"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create a Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-web"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
