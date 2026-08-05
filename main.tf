2fbb23ca671f4be796d44fd59c429728ec4c70b6
ntshuxeko shirinda <ntshuxekoshirinda96@gmail.com>
2026-08-05 13:23:06 +0200

    Add Azure resource group, app service plan, and linux web app

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-#Provider
+# Provider
 provider "azurerm" {
   features {}
 }
@@ -23,4 +23,29 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+# --- App Hosting Layer for miniMessageRender ---
+
+# Create an App Service Plan (Free F1 Tier)
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-minimessagerender"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+# Create the Linux Web App
+resource "azurerm_linux_web_app" "app" {
+  name                = "minimessagerender-app-prod"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.asp.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      node_version = "18-lts"
+    }
+  }
 }
